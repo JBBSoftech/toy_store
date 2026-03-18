41ab3a22a770198301fed121632113dadd003a19
JBBSoftech <jbbsoftech@gmail.com>
2026-03-18 14:52:36 +0530

    Update Appifyours/frontend/lib/main.dart

--- Appifyours/frontend/lib/main.dart
+++ Appifyours/frontend/lib/main.dart
@@ -1089,7 +1089,7 @@ class AdminManager {
 
       final response = await http.get(
 
-        Uri.parse('http://192.168.0.9:5000/api/admin/app-info'),
+        Uri.parse('http://192.168.0.3:5000/api/admin/app-info'),
 
         headers: {'Content-Type': 'application/json'},
 
@@ -1439,7 +1439,7 @@ class _SignInPageState extends State<SignInPage> {
 
       final response = await http.post(
 
-        Uri.parse('http://192.168.0.9:5000/api/login'),
+        Uri.parse('http://192.168.0.3:5000/api/login'),
 
         headers: {'Content-Type': 'application/json'},
 
